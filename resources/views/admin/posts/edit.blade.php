6e6b2ceb787ed8fa64273a4022e96c9549f93a6b
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-18 18:11:04 +0100

    Miglirorato edit

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -9,23 +9,20 @@
             @method("PUT")
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" class="form-control @error("title") is-invalid @enderror" id="title" name="title" placeholder="Inserisci il titolo" value="{{old("title") ? old("title") : $post->title}}">
+                <input type="text" class="form-control @error("title") is-invalid @enderror" id="title" name="title" placeholder="Inserisci il titolo" value="{{old("title", $post->title)}}">
                 @error("title")
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="content">Descrizione</label>
-                <textarea rows="5" type="text" class="form-control @error("content") is-invalid @enderror" id="content" name="content" placeholder="Descrivi il post">{{old("content") ? old("title") : $post->content}}}</textarea>
+                <textarea rows="5" type="text" class="form-control @error("content") is-invalid @enderror" id="content" name="content" placeholder="Descrivi il post">{{old("content", $post->content)}}}</textarea>
                 @error("content")
                     <div class="alert alert-danger">{{$message}}</div>
                 @enderror
             </div>
             <div class="form-group form-check">
-                @php
-                    $published = old("published") ? old("published") : $post->published;  
-                @endphp
-                <input type="checkbox" class="form-check-input @error("published") is-invalid @enderror" id="published" name="published" {{$published ? "checked" : ""}}>
+                <input type="checkbox" class="form-check-input @error("published") is-invalid @enderror" id="published" name="published" {{old("published", $post->published) ? "checked" : ""}}>
                 <label class="form-check-label" for="published">Pubblica</label>
                 @error("published")
                     <div class="alert alert-danger">{{$message}}</div>
